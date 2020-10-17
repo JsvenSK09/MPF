bcc392d9ec0649f87df91603ec67067f7f365177
B3b3k2 (JSK) <jsvenseptian00@gmail.com>
2020-10-17 21:59:57 +0700

    MPF repo

---
+++ AIPON_XSR/src/aipon_xsr/AIPON_XSR.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aipon_xsr;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author ASUS
+ */
+public class AIPON_XSR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new INTERFACE().setVisible(true);
+        
+            
+    }
+    
+}
